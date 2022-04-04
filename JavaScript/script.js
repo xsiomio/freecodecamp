b6b9eed70816a72339259e093e39b078ddc24a62
Azamat Tulendinov <Tulendinov.a.b@gmail.com>
2022-04-04 18:59:19 +0600

    \"

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,1 @@
-let a = 48;
-let b = 108;
-let c = 33;
-
-// Only change code below this line
-a /=12;
-b /=4;
-c /=11;
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
