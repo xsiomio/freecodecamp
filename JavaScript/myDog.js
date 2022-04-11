d729bf70920449155ce41952bea120d731fbfea3
Azamat Tulendinov <Tulendinov.a.b@gmail.com>
2022-04-11 19:42:39 +0600

    +

---
+++ JavaScript/myDog.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Bady",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Busya", "Borya", "Dady"]
+    // Only change code above this line
+};
